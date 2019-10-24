35a26e192cdf6776bc9aa901ddbc1321847dee4d
Lucas Messenger <lucas.c.messenger@gmail.com>
2019-10-23 22:27:12 -0400

    Add GPLv3 licensing and update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,20 @@ setup(name='vsplunk',
                         'visidata>=1.5.2'],
       description='Splunk App for Visidata',
       author=vsplunk.__author__,
+      url='https://www.github.com/layertwo/vsplunk',
       python_requires='>=3.6',
       scripts=['bin/vsplunk'],
       packages=['vsplunk'],
-      license='GPLv3')
+      license='GPLv3',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: Security'
+      ],
+      keywords=('splunk tabular data spreadsheet terminal curses visidata'))
